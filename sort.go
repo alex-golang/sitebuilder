5360eddc95c9e10a90a7ab6c7e13e05b3632f0ad
Jim Teeuwen <jimteeuwen@gmail.com>
2014-02-19 23:37:54 +0100

    Further refactoring, tweaks and cleanups.

--- sort.go
+++ sort.go
@@ -7,13 +7,15 @@ import (
 	"sort"
 )
 
+// PostsByDate sorts posts by date -- descending
 type PostsByDate []*Post
 
 func (p PostsByDate) Len() int           { return len(p) }
 func (p PostsByDate) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
-func (p PostsByDate) Less(i, j int) bool { return p[j].Date.After(p[i].Date) }
+func (p PostsByDate) Less(i, j int) bool { return p[i].Date.After(p[j].Date) }
 func (p PostsByDate) Sort()              { sort.Sort(p) }
 
+// PostsByTitle sorts posts by title -- alphabetically/ascending
 type PostsByTitle []*Post
 
 func (p PostsByTitle) Len() int           { return len(p) }
@@ -21,6 +23,7 @@ func (p PostsByTitle) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 func (p PostsByTitle) Less(i, j int) bool { return p[i].Title < p[j].Title }
 func (p PostsByTitle) Sort()              { sort.Sort(p) }
 
+// TagsByName sorts tags by name -- alphabetically/ascending
 type TagsByName []TagData
 
 func (p TagsByName) Len() int           { return len(p) }
